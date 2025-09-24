3dd8e2f9b39b10ee444252e229e02d2f520e3314
mtjf development <dev@mtjfoundation.org>
2025-09-24 15:43:48 +0500

    added mt to loader

--- src/components/global-components/loading_spinner/LoadingSpinner.js
+++ src/components/global-components/loading_spinner/LoadingSpinner.js
@@ -3,7 +3,7 @@ import './LoadingSpinner.css';
 
 const LoadingSpinner = ({ text = "Load&nbsp;ng", className = "" }) => {
     return (
-        <div className={`loading-spinner-container ${className}`}>
+        <div className={`loading-spinner-container ${className} mt-120`}>
             <span className="loader" dangerouslySetInnerHTML={{ __html: text }}></span>
         </div>
     );
