165538843dadb4069ece7212c7b9a7f433f4e58e
mtjf development <dev@mtjfoundation.org>
2025-09-26 17:23:25 +0500

    added meta and

--- src/components/donate.js
+++ src/components/donate.js
@@ -5,7 +5,6 @@ import Banner from './section-components/banner-v2';
 import CallToActionV1 from './section-components/call-to-action-v1';
 import Footer from './global-components/footer';
 import CategoryV2 from './section-components/category-v2';
-import DonationMenuForm from './section-components/donation_menu_form';
 import PageHeader from './global-components/page-header';
 
 // update this for donation page
@@ -45,8 +44,6 @@ const Donate  = () => {
         {/* <Banner /> */}
         <PageHeader  customclass="flood-bg-image" />
         <CategoryV2 showAllSections={true} />
-        <DonationMenuForm />
-
         {/* <CallToActionV1 />
          */}
          <Footer />
